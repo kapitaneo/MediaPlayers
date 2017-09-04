102b8d715bd14c4bb704ce818985a4f8061eecb9
Arty <Arty@DESKTOP-JKV2R0O>
2017-09-04 22:14:52 +0300

    interfaces

---
+++ MediaPlayers.Core/Interfaces/IProgressBar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayers.Core.Interfaces
+{
+    public interface IProgressBar
+    {
+        int ProgressBarCurrentTime { get; set; }
+        IMediaPlayer PlayerProgressBar { get; set; }
+        double GetTime();
+        void SetTime(double time);
+    }
+}
